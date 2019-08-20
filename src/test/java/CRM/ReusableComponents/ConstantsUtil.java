107d7686eb7c65d6e873aa850752e5c98f960844
gfl-jeevan <jteja@gflenv.com>
2019-08-20 17:31:43 -0400

    Added path variables to constants util. Added validation information messages as reusable components inside constantsutil

--- src/test/java/CRM/ReusableComponents/ConstantsUtil.java
+++ src/test/java/CRM/ReusableComponents/ConstantsUtil.java
@@ -1,5 +1,8 @@
 package CRM.ReusableComponents;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * @author jteja
  *
@@ -19,6 +22,8 @@ public class ConstantsUtil {
 	public static final String GLOBAL_LOADER = "global-loader mat-progress-bar";
 	public static final String OVERLAY_CONTAINER ="div[class='cdk-overlay-container'] div[class='cdk-overlay-connected-position-bounding-box'] div[class='cdk-overlay-pane']";
 	public static final String VALIDATION_MESSAGES = "div[class='cdk-overlay-container'] div[class='cdk-global-overlay-wrapper'] div[class='cdk-overlay-pane'] snack-bar-container";
+	public static final String VALIDATION_SUCCESS = "div[class='cdk-overlay-container'] div[class='cdk-global-overlay-wrapper'] div[class='cdk-overlay-pane'] snack-bar-container success-snack-bar";
+	public static final String VALIDATION_FAILURE = "div[class='cdk-overlay-container'] div[class='cdk-global-overlay-wrapper'] div[class='cdk-overlay-pane'] snack-bar-container validation-snack-bar";
 	public static final String DROPDOWN_OPTIONS = "mat-option";			
 	public static final String RADIO_OPTIONS = "mat-radio-button";
 	public static final String CHECKBOX_OPTIONS = "mat-checkbox";
@@ -26,6 +31,12 @@ public class ConstantsUtil {
 	public static final String DATA_OPTIONS = "td";
 	public static final String ANCHOR_OPTIONS = "a";
 
+	//Validation information messages - WebElements
+	@FindBy(css=VALIDATION_SUCCESS)
+	public static WebElement validation_success;
+	@FindBy(css=VALIDATION_FAILURE)
+	public static WebElement validation_failure;
+	
 	//Page Names
     public static final String ADMIN_SHEET = "Admin";
     public static final String AGREEMENT_CREATE_SHEET = "AgreementCreate";
